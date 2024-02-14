b077d81dbaeabec8ed829c8cf6aad03e8e7f3bbe
DESKTOP-QQHFIHU\ThietNguyen <nvthiet.it.95@gmail.com>
2024-02-14 14:44:40 +0700

    init commit

---
+++ src/test/java/com/xiao/merchants/MerchantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiao.merchants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MerchantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
